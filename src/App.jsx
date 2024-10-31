b2dcbe92b96c3e1e42b2a980c8160189f68d37aa
kiranashfaq <ahmedmemon4123@gmail.com>
2024-10-31 16:38:54 +0500

    quiz-app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Quiz from "./components/Quiz/Quiz"
+
+function App() {
+
+  return (
+    <>
+      <Quiz/>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/data.js
@@ -0,0 +1,93 @@
+ export const data = [
+    {
+      name: "Question 1",
+      question: "Which of the following is a JavaScript data type?",
+      option1: "Boolean",
+      option2: "Style",
+      option3: "HTML",
+      option4: "CSS",
+      ans: 1
+    },
+    {
+      name: "Question 2",
+      question: "How do you declare a JavaScript variable?",
+      option1: "var myVar",
+      option2: "variable myVar",
+      option3: "declare myVar",
+      option4: "v myVar",
+      ans: 1
+    },
+    {
+      name: "Question 3",
+      question: "What symbol is used to comment a single line in JavaScript?",
+      option1: "//",
+      option2: "/* */",
+      option3: "#",
+      option4: "<!-- -->",
+      ans: 1
+    },
+    {
+      name: "Question 4",
+      question: "Which method is used to find HTML elements by ID in JavaScript?",
+      option1: "getElementById()",
+      option2: "querySelectorAll()",
+      option3: "getElementsByClassName()",
+      option4: "getElementsByTagName()",
+      ans: 1
+    },
+    {
+      name: "Question 5",
+      question: "What is the result of '2' + 2 in JavaScript?",
+      option1: "4",
+      option2: "'4'",
+      option3: "22",
+      option4: "Error",
+      ans: 3
+    },
+    {
+      name: "Question 6",
+      question: "Which company developed JavaScript?",
+      option1: "Microsoft",
+      option2: "Mozilla",
+      option3: "Netscape",
+      option4: "Google",
+      ans: 3
+    },
+    {
+      name: "Question 7",
+      question: "Which keyword is used to define a constant in JavaScript?",
+      option1: "var",
+      option2: "const",
+      option3: "let",
+      option4: "constant",
+      ans: 2
+    },
+    {
+      name: "Question 8",
+      question: "Which method is used to add an element at the end of an array in JavaScript?",
+      option1: "push()",
+      option2: "pop()",
+      option3: "shift()",
+      option4: "unshift()",
+      ans: 1
+    },
+    {
+      name: "Question 9",
+      question: "What is the value of 'typeof NaN' in JavaScript?",
+      option1: "number",
+      option2: "NaN",
+      option3: "object",
+      option4: "undefined",
+      ans: 1
+    },
+    {
+      name: "Question 10",
+      question: "Which of the following is not a looping structure in JavaScript?",
+      option1: "for",
+      option2: "while",
+      option3: "foreach",
+      option4: "repeat",
+      ans: 4
+    }
+  ];
+  
